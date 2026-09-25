2ee54a0e8847e6d1383430cd8c5f671e3adcef32
felipe bastos <felipebastosweb@gmail.com>
2026-09-25 15:45:59 -0300

    Update Model.js

--- core/Model.js
+++ core/Model.js
@@ -1,12 +1,9 @@
 
 export class Model {
     constructor(self) {
-        this.id = (self.id != null)
-            ? self.id : null
-        this.create_date = (self.create_date != null)
-            ? self.create_date : Date.now()
-        this.update_date = (self.update_date != null)
-            ? self.update_date : Date.now()
+        this.id = (self.id != null) ? self.id : null
+        this.created_at = (self.created_at != null) ? self.created_at : Date.now()
+        this.updated_at = null
     }
 }
 
